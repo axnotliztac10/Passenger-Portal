e3f3806f30224cd457c293dd40517e1950850664
axnotliztac10 <axnotliztac@gmail.com>
2014-10-12 22:19:51 -0500

    Added missing files

---
+++ controllers/historyController.js
@@ -0,0 +1,16 @@
+angular.module('darkRide').controller('historyController', 
+    [
+        '$scope',
+        '$modal',
+        '$rootScope',
+        '$state',
+    function(
+        $scope,
+        $modal,
+        $rootScope,
+        $state
+    ) {
+
+
+
+}]);
