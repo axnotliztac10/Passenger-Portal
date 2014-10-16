0d1bab6e10da4d7dfead5d507f0551147c676939
axnotliztac10 <axnotliztac@gmail.com>
2014-10-15 23:36:43 -0500

    Second fix

--- factories/apiFactory.js
+++ factories/apiFactory.js
@@ -1,10 +1,10 @@
 angular.module('blackRide').factory('AuthFactory' , function ($resource, API_HOST) {
-	return $resource(API_HOST + '/auth', 
+	return $resource(API_HOST + '/auth', {} 
 		{
-			callback: "JSON_CALLBACK" 
-		}, 
-		{
-			save: { method: "JSONP" }
+			save: { method: "JSONP" },
+			params: {
+	          callback: 'JSON_CALLBACK'
+	        }
 		});
 });
 
