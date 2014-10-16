70b4414a41c799cd50201b361f8ab9834e2e0d22
axnotliztac10 <axnotliztac@gmail.com>
2014-10-15 23:49:51 -0500

    remove jsonp

--- factories/apiFactory.js
+++ factories/apiFactory.js
@@ -1,13 +1,5 @@
 angular.module('blackRide').factory('AuthFactory' , function ($resource, API_HOST) {
-	return $resource(API_HOST + '/auth', {},
-		{
-			save: { 
-				method: "JSONP",
-				params: {
-		          callback: 'JSON_CALLBACK'
-		        }
-			}
-		});
+	return $resource(API_HOST + '/auth');
 });
 
 angular.module('blackRide').factory('SignupFactory' , function ($resource, API_HOST) {
