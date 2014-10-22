8d973d368e979aa8be1f13152bfe65af8a89e8b8
axnotliztac10 <axnotliztac@gmail.com>
2014-10-22 00:55:00 -0500

    Visual improvemets, hstory filtering and ordering completed

--- app.js
+++ app.js
@@ -139,3 +139,34 @@ angular.module('blackRide').controller('modalConfirm', function ($rootScope, $sc
   });
 
 });
+
+angular.module('blackRide').filter('getCustomDate', function() {
+  return function(item) {
+    var date = new Date(item);
+      switch (date.getDay()) {
+          case 0:
+              day = "Sunday";
+          break;
+          case 1:
+              day = "Monday";
+          break;
+          case 2:
+              day = "Tuesday";
+          break;
+          case 3:
+              day = "Wednesday";
+          break;
+          case 4:
+              day = "Thursday";
+          break;
+          case 5:
+              day = "Friday";
+          break;
+          case 6:
+              day = "Saturday";
+          break;
+      }
+
+      return day + ", " + item;
+  };
+});
--- controllers/historyController.js
+++ controllers/historyController.js
@@ -11,6 +11,31 @@ angular.module('blackRide').controller('historyController',
         $state
     ) {
 
+        $scope.els = [
+            {
+                departure: 'Mission St. 125',
+                destiny: 'Oak St',
+                date: '2014-04-11',
+                price: 24
+            },
+            {
+                departure: 'Mission St. 125',
+                destiny: 'Oak St',
+                date: '2014-04-12',
+                price: 79
+            },
+            {
+                departure: 'Mission St. 125',
+                destiny: 'other address',
+                date: '2014-04-13',
+                price: 23
+            }
+        ];
 
+        $scope.sorts = [
+            {value: "date", label: "Time"},
+            {value: "price", label: "Price"}
+        ];
 
+        $scope.sort = $scope.sorts[0];
 }]);
