2d2b2049f9313744f222c22459309dc7b0b28185
luis.lopez <luis.lopez.adesis@gmail.com>
2015-01-27 00:56:58 -0600

    Configure grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
             },
             build: {
                 cwd: "build/",
-                src: "**"
+                src: "**/*"
             }
         },
         copy: {
