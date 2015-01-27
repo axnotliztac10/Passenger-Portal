3fce2efb434d13dcdf1104216c7e7bda9bac543b
luis.lopez <luis.lopez.adesis@gmail.com>
2015-01-27 00:54:52 -0600

    Added deployer aws

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,11 +26,32 @@ module.exports = function(grunt) {
                 },
                 files: {"assets/css/app.css": "assets/css/app.less"}
             }
+        },
+        aws: grunt.file.readJSON("credentials.json"),
+        s3: {
+            options: {
+                accessKeyId: "<%= aws.accessKeyId %>",
+                secretAccessKey: "<%= aws.secretAccessKey %>",
+                bucket: "shiftportal"
+            },
+            build: {
+                cwd: "build/",
+                src: "**"
+            }
+        },
+        copy: {
+            bower: { files: [{ expand: true, cwd: 'bower_components/', src: ['**/*'], dest: 'build/bower_components'}] },
+            assets: { src: 'assets/css/*', dest: 'build/' },
+            index: { src: 'index.html', dest: 'build/'},
+            app: { files: [{ expand: true, cwd: 'app/', src: ['**/*'], dest: 'build/app'}] }
         }
-
     });
 
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-aws');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-less');
+
     grunt.registerTask('default', ['less', 'connect']);
+    grunt.registerTask('deploy', ['copy', 's3']);
 };
